03f8c2b8458c0bfc8b2131e834bba54bb87810d1
zhuk76787 <zhuk76787@gmail.com>
2023-12-20 17:37:47 +0400

    sprint-3

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var count = 0
+    @IBOutlet weak var lable: UILabel!
+    
+    @IBOutlet weak var button: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func countButton(_ sender: Any) {
+       count += 1
+        self.lable.text = "Значение счетчика: \(count)"
+    }
+    
 }
 
